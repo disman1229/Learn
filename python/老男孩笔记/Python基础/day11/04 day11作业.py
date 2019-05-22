625a09872191324ec296bf0e40e2a15a27ba3b59
disman1229 <346025232@qq.com>
2019-05-22 14:39:01 +0800

    day11作业完成到12题

--- python/老男孩笔记/Python基础/day11/04 day11作业.py
+++ python/老男孩笔记/Python基础/day11/04 day11作业.py
@@ -45,3 +45,91 @@
 # def func(**kwargs):
 #     print(kwargs)
 # func(**{'name':'disman'},**{'age':34})
+
+# 7.下面代码成立吗?如果不成立为什么报错?怎么解决?
+# 7.1
+# a = 2
+# def wrapper():
+#     print(a)
+# wrapper()
+# 成立  ,结果为2
+
+# 7.2
+# a = 2
+# def wrapper():
+#     global a
+#     a += 1
+#     print(a)
+# wrapper()
+# 不成立,局部变量不能修改全局变量,需要加入global
+
+# 7.3
+# def wrapper():
+#     a = 1
+#     def inner():
+#         print(a)
+#     inner()
+# wrapper()
+# 成立,结果为1
+
+# 7.4
+# def wrapper():
+#     a = 1
+#     def inner():
+#         nonlocal a # 需要找上次变量a
+#         a += 1
+#         print(a)
+#     inner()
+# wrapper()
+# 不成立,局部变量内没有找到变量a,需要加入nonlocal,找上一层变量a
+
+# 8.写函数,接收两个参数并把比较小的数返回
+# def func(a,b):
+#     return a if a < b else b
+# print(func(1,2))
+
+
+# 9.写函数,接受一个参数(此参数类型必须是可迭代对象),将可迭代对象每个元素以"_"相连接,形成新的字符串,并返回
+# 例如:传入可迭代对象为[1,"鲁班七号",2,"程咬金"]返回结果为"1_鲁班七号_2_程咬金"
+
+# def func(lst):
+#     s = ""
+#     for el in lst:
+#         s = s + str(el) + "_"
+#     return s.rstrip("_")
+# print(func([1,"鲁班七号",2,"程咬金"]))
+
+# 10.写函数,传入n个数,返回字典{"max":最大值,"min":最小值}
+# 例如:min,max(2,5,7,8,4)返回{"max":8,min:2}(此题用到max(),min()内置函数)
+# def func(*args):
+#     return {"max":max(args),"min":min(args)}
+# print(func(2,3,4,1,567,234,123,6,457435,212,3123))
+
+# 11.写函数,传入一个函数n,返回n的阶乘
+# 例如:cal(7) 计算7*6*5*4*3*2*1
+# def func(n):
+#     sum = 1
+#     for i in range(n):
+#         sum = sum * (i+1)
+#     return sum
+# print(func(12))
+
+# def func(n):
+#     sum = 1
+#     while n >= 1:
+#         sum = sum * n
+#         n -= 1
+#     return sum
+# print(func(5))
+
+# 12.写函数,返回一个扑克列表,里面有52项,每一项是一个元祖
+# 例如:[("红心",2),("草花",2)...("黑桃",1)]
+# def func():
+#     result = []
+#     huase = ["红心","黑桃","草花","方片"]
+#     dianshu = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+#     for hua in huase:
+#         for dian in dianshu:
+#             result.append((hua,dian))
+#     return result
+# print(func())
